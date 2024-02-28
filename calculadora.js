690a16e3d783ed005fde3ec4946d0a3400bdaf22
andreyhenrique05 <132684929+andreyhenrique05@users.noreply.github.com>
2024-02-28 09:45:39 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,52 @@
+function somar(){
+      const num1 = 10;
+      const num2 = 20;
+      
+      const Total = num1 + num2;
+
+      console.log(`A soma ${num1} + ${num2} = ${total}`);
+}
+
+function somar(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Total = num1 + num2;
+
+      console.log(`A divisão ${num1} + ${num2} = ${totaldiv}`);
+}
+
+function somar(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Total = num1 + num2;
+
+      console.log(`A A multiplicação ${num1} + ${num2} = ${totalmul}`);
+}
+
+function somar(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const Total = num1 + num2;
+
+      console.log(`A subtração ${num1} + ${num2} = ${totalsub}`);
+}
+
+function somar2 (num1, num2){
+    const total= num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} = ${total}`);
+}
+
+function somar3 (num1, num2){
+    const num1 = 10;
+    const num2 = 20;
+
+    const total= num1 + num2;
+    
+    console.log(`A soma ${num1} + ${num2} = ${total}`);
+
+    
+}
