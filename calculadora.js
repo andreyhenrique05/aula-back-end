21fbc246d9ca257d0044105a1c1f3ffb9f32ccd5
andreyhenrique05 <132684929+andreyhenrique05@users.noreply.github.com>
2024-02-28 12:23:05 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -47,6 +47,17 @@ function somar3 (num1, num2){
     const total= num1 + num2;
     
     console.log(`A soma ${num1} + ${num2} = ${total}`);
-
-    
 }
+
+//chamando as funções para executar-las 
+ somar2();
+ somar3();
+
+ // agora que conhecemos as funções, vms criar uma calculadora  em javascript.
+ // crie 4 funções, das operações de soma, multiplicação, divisão e subtração 
+
+ // quais são ps operadores de soma, multiplicação, divisão e subtração 
+// + (soma)
+// - (subtração)
+// * (multiplicação)
+// /  (divisão)
